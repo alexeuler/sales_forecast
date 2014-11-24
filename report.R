10a9632e0fdf38616b6cd44f5890e77c08cf3456
alex <akarasev@nrgc.com>
2014-11-24 19:11:52 +0300

    finalized

--- report.R
+++ report.R
@@ -27,10 +27,13 @@ make_forecast = function (data, time, period, model) {
   return(list(
     count = report_plot(forecast_count[[time]], "Customers"),
     spend = report_plot(forecast_spend[[time]], "Average ticket"),
-    revenue = report_plot(forecast_revenue[[time]], "Total income")))
+    revenue = report_plot(forecast_revenue[[time]], "Total income"),
+    fcount = forecast_count,
+    fspend = forecast_spend,
+    frevenue = forecast_revenue))
 }
 
-make_slide = function(data, title) {
+make_slide = function(data, title, subtitle) {
 
   df = data.frame()
   title = ggplot(df) + geom_point() + 
@@ -43,24 +46,32 @@ make_slide = function(data, title) {
           panel.grid.major = element_blank(),
           panel.grid.minor = element_blank(),
           panel.border = element_blank(),
-          panel.background = element_blank(),
-          plot.title = element_text(vjust=0.5)) +
-    annotate("text", x = 5, y = 5, label = title, size=15)
+          panel.background = element_blank()) +
+    annotate("text", x = 5, y = 5, label = title, size=15) + 
+    annotate("text", x = 5, y = 1, label = subtitle, size=7)
   
-  grid.arrange(title, arrangeGrob(data$count, data$spend, ncol=2), data$revenue, heights = c(1,4,4))
+  grid.arrange(title, arrangeGrob(data$count, data$spend, ncol=2), data$revenue, heights = c(1,3,3))
  
 }
 
 
 current_week_plot = make_forecast(cust, "currentf", "daily", MODEL)
 next_week_plot = make_forecast(cust, "nextf", "daily", MODEL)
-
 current_quarter_plot = make_forecast(cust_weekly, "currentf", "weekly", MODEL)
 next_quarter_plot = make_forecast(cust_weekly, "nextf", "weekly", MODEL)
 
-pdf("plots.pdf", onefile = TRUE, width = 16, height = 16 / 297 * 210)
-make_slide(current_week_plot, "Current Week")
-make_slide(next_week_plot, "Next Week")
-make_slide(current_quarter_plot, "Current Quarter")
-make_slide(next_quarter_plot, "Next Quarter")
+current_week_subtitle = paste("(", min(current_week_plot$fcount$currentf$date), " - ", max(current_week_plot$fcount$currentf$date), ")", sep="")
+next_week_subtitle = paste("(", min(next_week_plot$fcount$nextf$date), " - ", max(next_week_plot$fcount$nextf$date), ")", sep="")
+current_quarter_subtitle = paste("(", min(current_quarter_plot$fcount$currentf$date), " - ", max(current_quarter_plot$fcount$currentf$date) + 6, ")", sep="")
+next_quarter_subtitle = paste("(", min(next_quarter_plot$fcount$nextf$date), " - ", max(next_quarter_plot$fcount$nextf$date) + 6, ")", sep="")
+
+
+time = gsub("[:]","-",Sys.time())
+time = gsub("[ ]",", ",time)
+name = paste(getwd(),"/Reports/Report ",time, ".pdf", sep="")
+pdf(name, onefile = TRUE, width = 16, height = 16 / 297 * 210)
+make_slide(current_week_plot, "Current Week", current_week_subtitle)
+make_slide(next_week_plot, "Next Week", next_week_subtitle)
+make_slide(current_quarter_plot, "Current Quarter", current_quarter_subtitle)
+make_slide(next_quarter_plot, "Next Quarter", next_quarter_subtitle)
 dev.off()
